d640660d82b1018624fa2e69074feefccd0a6533
mmahapat <mmahapat@asu.edu>
2019-11-25 01:58:13 -0700

    HomePage CSS

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import org.apache.xerces.dom.ChildNode;
 
 import java.io.IOException;
 
@@ -15,10 +16,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.searchStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("SearchPage.fxml"));
+        root.setId("pane");
         primaryStage.setTitle("Library Management System");
-        primaryStage.setScene(new Scene(root, 500, 275));
-        openSearchWindow();
+        Scene scene = new Scene(root, 500, 500);
+        scene.getStylesheets().addAll(getClass().getClassLoader().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
     public void openSearchWindow() throws IOException {
@@ -26,6 +32,7 @@ public class Main extends Application {
         GridPane pane = loader.load();
         Scene scene = new Scene(pane);
         searchStage = new Stage();
+        scene.getStylesheets().addAll(getClass().getClassLoader().getResource("style.css").toExternalForm());
         searchStage.setScene(scene);
         searchStage.show();
     }
